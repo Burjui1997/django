e716c9c727437123745e0c702582cc935137afa6
burjui <burjui.dota@mail.ru>
2024-02-17 01:26:18 +0300

    Start

---
+++ cards/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('catalog/', views.catalog, name='catalog'),
+
+    path('catalog/<int:card_id>/', views.get_card_by_id, name='card'),
+
+    path('catalog/<slug:slug>/', views.get_category_by_name, name='category'),
+
+]
